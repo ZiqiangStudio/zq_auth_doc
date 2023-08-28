e8c315f53f9beab808bb86e6ec0da0b11980951a
Nagico <yjr888@vip.qq.com>
2023-08-28 21:07:28 +0800

    fix: 更新github地址

--- docs/.vitepress/scripts/meta.ts
+++ docs/.vitepress/scripts/meta.ts
@@ -5,7 +5,7 @@ export const developerName = "自强 Studio";
 
 export const title = "自强 Auth 文档";
 
-export const github = 'https://github.com/dapandocs/danpan-docs';
+export const github = 'https://github.com/ZiqiangStudio/zq_auth_doc';
 
 export const keywords = "自强Studio，自强Auth";
 
